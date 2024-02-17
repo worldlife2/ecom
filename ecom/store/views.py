e71dd31b30ccd7aa9935ce4416b13a1422b5822c
worldlife2 <worldlife-2@hotmail.com>
2024-02-16 19:21:37 -0500

    adding search view

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -6,10 +6,26 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
-
+from django.db.models import Q
 
 # Create your views here.
 
+def search(request):
+    #Determine if they filled out the form
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        # Query The Products DB Model
+        # searched = Product.objects.filter(name__contains=searched)
+        searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+        # Test for null
+        if not searched:
+            messages.success(request, ("Sorry That Product Does Not Exist. Please Try Again ..."))
+            return render(request, 'search.html', {})
+        else:
+            return render(request, 'search.html', {'searched':searched})           
+    else:
+        return render(request, 'search.html', {})
+    
 
 def category_summary(request):
     categories = Category.objects.all()
